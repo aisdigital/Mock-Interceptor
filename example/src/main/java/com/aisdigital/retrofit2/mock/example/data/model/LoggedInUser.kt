5f3eb28ab5a1cc3229ef13f0b997c5075308a905
Natan <xpenatan@gmail.com>
2020-02-17 10:41:12 -0300

    First commit

---
+++ example/src/main/java/com/aisdigital/retrofit2/mock/example/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.aisdigital.retrofit2.mock.example.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ example/src/main/java/com/aisdigital/retrofit2/mock/example/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.aisdigital.retrofit2.mock.example.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
